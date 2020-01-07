cfe4a646be9a14ad0221f44fd9b95e729c65d0fa
zxk7516 <zxk7516@foxmail.com>
2020-01-08 00:29:22 +0800

    qer

--- src/entries/user.rs
+++ src/entries/user.rs
@@ -1,5 +1,40 @@
+#[derive(Deserialize)]
+pub struct UserList {
+    page: i64,
+    num: i64,
+    name: Option<String>,
+}
+
+#[post("/api/users")]
+async fn index(db: Data<MysqlPool>, req: Json<UserList>) -> impl Responder {
+    use crate::schema::users::dsl::*;
+    let conn = db.get().unwrap();
+    let offset = (req.page - 1) * req.num;
+
+    let limited = users.limit(req.num).offset(req.num);
+    let data;
+    let q_name;
+    if req.name.is_some() && {
+        q_name = req.name.as_ref().unwrap();
+        q_name.len() > 0
+    } {
+        let q_name = req.name.as_ref().unwrap();
+        data = limited
+            .filter(name.like(format!("%{}%", q_name)))
+            .offset(offset)
+            .load::<User>(&conn)
+            .expect("error fetch data");
+    } else {
+        data = limited
+            .offset(offset)
+            .load::<User>(&conn)
+            .expect("error fetch data");
+    }
+    Json(data)
+}
+
 use actix_web::{
-    get,
+    get, post,
     web::{Data, Json},
     Responder,
 };
@@ -8,15 +43,4 @@ use diesel::prelude::*;
 use crate::models::user::*;
 use crate::MysqlPool;
 
-#[get("/api/users")]
-async fn index(db: Data<MysqlPool>) -> impl Responder {
-    use crate::schema::users::dsl::*;
-    let conn = db.get().unwrap();
-
-    Json(
-        users
-            .limit(10)
-            .load::<User>(&conn)
-            .expect("error fetch data"),
-    )
-}
+use serde::Deserialize;
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,9 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(Queryable, Debug, Serialize)]
 pub struct User {
     pub id: i32,
     pub name: String,
     pub email: String,
+    #[serde(skip)]
     pub password: String,
 }
