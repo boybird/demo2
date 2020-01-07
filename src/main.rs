77cdc9c79f9e9f4611ab7958fc0af8499362e09f
zxk7516 <zxk7516@foxmail.com>
2020-01-07 14:39:49 +0800

    setup db connection

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
 // include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
-fn esteblish_connection() {}
+fn esteblish_connection() -> r2d2::Pool<ConnectionManager<MysqlConnection>> {
+    let db_url: String = std::env::var("DATABASE_URL").expect("请设置数据库链接");
+    let db_manager = ConnectionManager::<MysqlConnection>::new(db_url);
+
+    r2d2::Pool::new(db_manager).expect("Failed to create pool")
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    esteblish_connection();
+    let _ = dotenv::dotenv();
 
     HttpServer::new(move || {
         // let generated = generate();
-        App::new().service(entries::home::index)
+        let pool = esteblish_connection();
+
+        App::new()
+            // data
+            .data(pool)
+            // routes
+            .service(entries::home::index)
         // .service(actix_web_static_files::ResourceFiles::new("", generated))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(std::env::var("HOST_PORT").unwrap_or("127.0.0.1:8080".to_owned()))?
     .run()
     .await
 }
 
-lazy_static! {
-    static ref _ENV: () = {
-        let _ = dotenv::dotenv();
-    };
-    static ref DATABASE_URL: String = std::env::var("DATABASE_URL").expect("请设置数据库链接");
-    static ref HOST_PORT: String =
-        std::env::var("HOST_PORT").unwrap_or("127.0.0.1:8080".to_owned());
-}
+// lazy_static! {
+    // static ref _ENV: () = {
+        // let _ = dotenv::dotenv();
+    // };
+    // static ref DATABASE_URL: String = std::env::var("DATABASE_URL").expect("请设置数据库链接");
+    // static ref HOST_PORT: String =        std::env::var("HOST_PORT").unwrap_or("127.0.0.1:8080".to_owned());
+    // static ref DB_POOL: r2d2::Pool<diesel::r2d2::ConnectionManager<MysqlConnection>> =
+
+// }
 
 #[macro_use]
 extern crate diesel;
-#[macro_use]
-extern crate lazy_static;
+// #[macro_use]
+// extern crate lazy_static;
 use actix_web::{App, HttpServer};
 use diesel::prelude::*;
-use diesel::r2d2;
+use diesel::r2d2::ConnectionManager;
 
 // use actix_web_static_files;
 // use std::collections::HashMap;
